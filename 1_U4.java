bb39064313714ab334a98f2aacf805babdc41530
danstort <129515419+danstort@users.noreply.github.com>
2023-11-10 13:04:16 +0100

    Create 1_U4.java

---
+++ 1_U4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        
+        Scanner tec = new Scanner(System.in);
+        int ar [] = new int[10];
+        
+       for (int i=0; i<ar.length; i++) {
+        System.out.print(ar[i]+" ");
+       }
+       
+    }
+}
