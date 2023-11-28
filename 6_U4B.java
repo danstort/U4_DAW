9cfba5420034e7a9135c0df6e9761419a277b3a3
danstort <129515419+danstort@users.noreply.github.com>
2023-11-28 18:49:28 +0100

    Create 6_U4B.java

---
+++ 6_U4B.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/**
+ *
+ * @author danielsotoortiz
+ */
+public class Ejercicio6 {
+    
+    public static void main(String[] args) {
+        
+        Scanner tec = new Scanner(System.in);
+        
+        String frase = tec.nextLine();
+        
+        int esp = 0;
+        
+       
+       
+           frase=frase.replace(" ", "");
+           
+       
+          
+        System.out.print("Frase: "+frase);
+    }
+    
+}
